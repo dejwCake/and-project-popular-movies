e659a03e4163e02eee8dc9b1972fc927faa706f8
David Běhal <behal.david@gmail.com>
2018-03-03 23:29:10 +0100

    Prepared grid view

--- app/src/main/java/sk/dejw/android/popularmovies/MainActivity.java
+++ app/src/main/java/sk/dejw/android/popularmovies/MainActivity.java
@@ -3,16 +3,39 @@ package sk.dejw.android.popularmovies;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.GridView;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GridView mGridView;
+    private ProgressBar mLoadingIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGridView = (GridView) findViewById(R.id.gw_posters);
+        mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
+
+        //TODO get data from remote host in async task
+
+        //TODO set adapter for gridview
+
+//        gridview.setAdapter(new ImageAdapter(this));
+//
+//        gridview.setOnItemClickListener(new OnItemClickListener() {
+//            public void onItemClick(AdapterView<?> parent, View v,
+//                                    int position, long id) {
+//                Toast.makeText(HelloGridView.this, "" + position,
+//                        Toast.LENGTH_SHORT).show();
+//            }
+//        });
     }
 
     @Override
